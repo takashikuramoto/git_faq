325c9bd76913a5d0d62ba695a5bd5eb267f94275
EC2 Default User <ec2-user@ip-172-31-45-96.ap-northeast-1.compute.internal>
2020-06-11 09:50:48 +0000

    questionモデルを作成

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+  
+  has_one_attached :image
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  def name
+    "#{family_name} #{first_name}"
+  end
+
+  def name_kana
+    "#{family_name_kana} #{first_name_kana}"
+  end
+  
+  def full_profile?
+    image.attached?  && family_name? && first_name? && family_name_kana? && first_name_kana?
+  end
+end
