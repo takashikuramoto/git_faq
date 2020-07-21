291889aff66c1dae63e0fc0b78e5d6cb59aff1bc
EC2 Default User <ec2-user@ip-172-31-45-96.ap-northeast-1.compute.internal>
2020-07-21 11:46:29 +0000

    add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+User.create(email: 'tech-camp@gmail.com', password: '12345678', nickname: 'Mako')
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
